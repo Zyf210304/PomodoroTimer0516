543817b65ad72c4bede5b1e39a5cfd2cbf0e13e6
张亚飞 <672233580@qq.com>
2022-05-17 17:36:49 +0800

    完成

--- PomodoroTimer0516/PomodoroTimer0516App.swift
+++ PomodoroTimer0516/PomodoroTimer0516App.swift
@@ -11,10 +11,34 @@ import SwiftUI
 struct PomodoroTimer0516App: App {
     //mark since we're doing background fetching intializing here
     @StateObject var pomodoroModel: PomodoroModel = .init()
+    
+    @Environment(\.scenePhase) var phase
+    @State var lastActiveTimeStamp: Date = Date()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(pomodoroModel)
+        }
+        .onChange(of: phase) { newValue in
+            
+            if pomodoroModel.isStarted {
+                if newValue == .background {
+                    lastActiveTimeStamp = Date()
+                }
+                
+                if newValue == .active {
+                    let currentTimeStampDiff = Date().timeIntervalSince(lastActiveTimeStamp)
+                    
+                    if pomodoroModel.totalSeconds - Int(currentTimeStampDiff) <= 0 {
+                        pomodoroModel.isStarted = false
+                        pomodoroModel.totalSeconds = 0
+                        pomodoroModel.isfinished = true
+                    } else {
+                        pomodoroModel.totalSeconds -=  Int(currentTimeStampDiff)
+                    }
+                }
+            }
         }
     }
 }
